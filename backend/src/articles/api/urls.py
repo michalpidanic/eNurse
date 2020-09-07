1768fd7dbbffe10550bf5b251c63a6e31ada3aa0
Tanczos Tomas <tanczost23@gmail.com>
2020-09-07 18:28:07 +0200

    re-created backend

--- backend/src/articles/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import ArticleDetailView, ArticleListView
-
-
-urlpatterns = [
-    path('', ArticleListView.as_view()),
-    path('<pk>', ArticleDetailView.as_view())
-]
