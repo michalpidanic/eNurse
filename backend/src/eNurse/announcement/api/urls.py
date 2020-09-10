32834769aed7a8b664705f54dee153259715b729
tanczost <tanczost23@gmail.com>
2020-09-10 13:20:27 +0200

    delete listApiView and RetrieveView, added View set to create, update and delete

--- backend/src/eNurse/announcement/api/urls.py
+++ backend/src/eNurse/announcement/api/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path, include
 
-from .views import AnnouncementListView
+#from .views import AnnouncementListView
 
-urlpatterns = [
+
+from announcement.api.views import AnnouncementViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'', AnnouncementViewSet, basename='announcement')
+urlpatterns = router.urls
+
+""" urlpatterns = [
     path('', AnnouncementListView.as_view())
 
 ]
+ """
--- backend/src/eNurse/announcement/api/views.py
+++ backend/src/eNurse/announcement/api/views.py
@@ -1,7 +1,20 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import viewsets
 
 from announcement.models import Announcement
 from .serializers import AnnouncementsSerializer
+
+
+class AnnouncementViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+    serializer_class = AnnouncementsSerializer
+    queryset = Announcement.objects.all()
+
+
+""" from rest_framework.generics import ListAPIView, RetrieveAPIView
+
+
 
 
 class AnnouncementListView(ListAPIView):
@@ -12,3 +25,4 @@ class AnnouncementListView(ListAPIView):
 # class AnnouncementDetailView(RetrieveAPIView):
 #    queryset = Announcement.objects.all()
 #    serializer_class = AnnouncementsSerializer
+ """
